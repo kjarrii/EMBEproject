60755a0034273be39c922fd793cb8209766fb5f6
kjartanfh19@ru.is <kjartanfh19@ru.is>
2024-11-06 05:07:39 +0000

    lsjkad

--- P5/RpiCode/part2_2.c
+++ P5/RpiCode/part2_2.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+/*#include <stdio.h>
 #include <stdlib.h> 
 #include <fcntl.h>
 #include <unistd.h>
@@ -105,6 +105,126 @@ int main(int argc, char *argv[]) {
     send_request(fd, server, func, reg, value);
     usleep(500000);
     read_response(fd);
+
+    close(fd);
+    return 0;
+}
+*/
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <errno.h>
+
+uint16_t ModRTU_CRC(uint8_t buf[], int len) {
+    uint16_t crc = 0xFFFF;
+    for (int pos = 0; pos < len; pos++) {
+        crc ^= (uint16_t)buf[pos];
+        for (int i = 8; i != 0; i--) {
+            if ((crc & 0x0001) != 0) {
+                crc >>= 1;
+                crc ^= 0xA001;
+            } else {
+                crc >>= 1;
+            }
+        }
+    }
+    return crc;
+}
+
+int open_serial(const char *device, int baudrate) {
+    int fd = open(device, O_RDWR | O_NOCTTY | O_SYNC);
+    if (fd < 0) return fd;
+    
+    struct termios tty;
+    memset(&tty, 0, sizeof tty);
+    if (tcgetattr(fd, &tty) != 0) return -1;
+
+    cfsetospeed(&tty, baudrate);
+    cfsetispeed(&tty, baudrate);
+
+    tty.c_cflag = (tty.c_cflag & ~CSIZE) | CS8;
+    tty.c_iflag &= ~IGNBRK;
+    tty.c_lflag = 0;
+    tty.c_oflag = 0;
+    tty.c_cc[VMIN] = 0;
+    tty.c_cc[VTIME] = 5;
+
+    tty.c_iflag &= ~(IXON | IXOFF | IXANY);
+    tty.c_cflag |= (CLOCAL | CREAD);
+    tty.c_cflag &= ~(PARENB | PARODD);
+    tty.c_cflag &= ~CSTOPB;
+    tty.c_cflag &= ~CRTSCTS;
+
+    if (tcsetattr(fd, TCSANOW, &tty) != 0) return -1;
+    return fd;
+}
+
+void send_modbus_request(int fd, uint8_t address, uint8_t function, uint16_t reg, uint16_t value) {
+    uint8_t request[8];
+    request[0] = address;
+    request[1] = function;
+    request[2] = reg >> 8;
+    request[3] = reg & 0xFF;
+    request[4] = value >> 8;
+    request[5] = value & 0xFF;
+    
+    uint16_t crc = ModRTU_CRC(request, 6);
+    request[6] = crc & 0xFF;
+    request[7] = crc >> 8;
+
+    write(fd, request, 8);
+}
+
+int read_modbus_response(int fd, uint8_t *response, int len) {
+    int total = 0;
+    while (total < len) {
+        int n = read(fd, response + total, len - total);
+        if (n > 0) total += n;
+        else if (n < 0) return -1;
+    }
+    return total;
+}
+
+void print_modbus_response(uint8_t *response, int length) {
+    for (int i = 0; i < length; i++) {
+        printf("%02X ", response[i]);
+    }
+    printf("\n");
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 5) {
+        printf("Usage: %s <address> <function> <register> <value>\n", argv[0]);
+        return 1;
+    }
+
+    uint8_t address = (uint8_t)strtoul(argv[1], NULL, 0);
+    uint8_t function = (uint8_t)strtoul(argv[2], NULL, 0);
+    uint16_t reg = (uint16_t)strtoul(argv[3], NULL, 0);
+    uint16_t value = (uint16_t)strtoul(argv[4], NULL, 0);
+
+    int fd = open_serial("/dev/ttyS0", B115200);
+    if (fd < 0) {
+        perror("Error opening serial port");
+        return 1;
+    }
+
+    send_modbus_request(fd, address, function, reg, value);
+
+    uint8_t response[8];
+    int length = read_modbus_response(fd, response, 8);
+
+    if (length < 0) {
+        perror("Error reading response");
+    } else {
+        printf("Received reply: ");
+        print_modbus_response(response, length);
+    }
 
     close(fd);
     return 0;
